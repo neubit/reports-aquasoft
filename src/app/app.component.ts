dd91730d91b345a7b01fef461224fcb6403abc89
rubenhdezz <ruben_ahs@hotmail.com>
2025-04-27 11:22:23 -0700

    Agregar auth

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { NzIconModule } from 'ng-zorro-antd/icon';
 import { NzLayoutModule } from 'ng-zorro-antd/layout';
 import { NzTypographyModule } from 'ng-zorro-antd/typography';
 import { NzFlexModule } from 'ng-zorro-antd/flex';
 import { NzSegmentedModule } from 'ng-zorro-antd/segmented';
+import { NzAlertModule } from 'ng-zorro-antd/alert';
 
 import { PdfViewerComponent } from './pages/pdf-viewer/pdf-viewer.component';
 import { FilterFormComponent } from './pages/filter-form/filter-form.component';
@@ -16,13 +17,19 @@ import { FilterFormComponent } from './pages/filter-form/filter-form.component';
     NzTypographyModule,
     NzFlexModule,
     NzSegmentedModule,
+    NzAlertModule,
     PdfViewerComponent,
     FilterFormComponent,
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   isCollapsed = false;
   isLoading = false;
+  inIframe = false;
+
+  ngOnInit(): void {
+    this.inIframe = window.self !== window.top;
+  }
 }
